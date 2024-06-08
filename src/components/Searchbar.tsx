b63169be50d994e8fefae079638cb5d153f95328
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-06-09 00:06:36 +0200

    fix bug in search bar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -185,9 +185,8 @@ const handleCategorySelect = (category: string) => {
             value={searchQuery}
             onChange={handleSearch}
             className="block p-4 w-full z-20 text-lg text-slate-700 bg-zinc-300 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-customCyan focus:border-customCyan"
-            placeholder="Search by location or business keywords"
-            disabled={category === "Choose a category"}
-            required
+            placeholder="Choose to search location or business keywords"
+            
           />
 
           {/* Clear Button */}
