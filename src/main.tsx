4c54d5518c1825c1c12b32bebcbb36dae1ca2f39
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-06-13 13:43:56 +0200

    add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context.tsx";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProviderWrapper>
         <App />
+        <SpeedInsights />
       </AuthProviderWrapper>
     </BrowserRouter>
   </React.StrictMode>
